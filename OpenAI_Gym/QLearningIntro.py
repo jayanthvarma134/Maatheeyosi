ec973c7497eb75f79024f43244a4f2b5f8b63651
jayanthvarma134 <jayanthvarma134@gmail.com>
2020-05-23 23:56:17 +0530

    Update QLearningIntro.py
    updating QLearningIntro.py(#02)

--- OpenAI_Gym/QLearningIntro.py
+++ OpenAI_Gym/QLearningIntro.py
@@ -5,3 +5,11 @@ env_name = 'CartPole-v1'
 env = gym.make(env_name)
 
 print('obsrvation space n :', env.observation_space)
+
+env.reset()
+
+for _ in range(200):
+	action = env.action_space.sample()
+	env.step(action)
+	env.render()
+
